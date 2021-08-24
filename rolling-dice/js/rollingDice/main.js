522809f662430340c4f46affd99d4df72e3b7f20
Jalon Duncan <duncanjalon@gmail.com>
2021-08-24 13:46:07 -0500

    must change integers to strings

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -14,42 +14,46 @@ for (let i = 0; i < 10; i++) {
     let die1 = RandRoll(1, 6);
     let die2 = RandRoll(1, 6);
 
-    let message = `${die1.toString()} + ${die2.toString()} ==${die1 + die2}`
-    if (die1 = die2)
-        return message += " DOUBLES"
-}
+    let message = `${die1.toString()} + ${die2.toString()} == ${die1 + die2}`
+    if (die1 === die2) {
+        message += " DOUBLES!"
+    }        
+        console.log(message)
+    }
 
-console.log(message)
 
-// Current state prints message but does not compute numbers
 
+
+
+// Current state prints message and integers but not numbers as strings
 
 
 // const dieToString = () => {
 
 //     let dieString = ""
 
-// if ( Roll === 1){
-//     dieString = "one";
-// }
-// else if ( Roll === 2){
-//     dieString = "two";
-// }
-// else if ( Roll === 3){
-//     dieString = "three";
-// }
-// else if ( Roll === 4){
-//     dieString = "four";
-// }
-// else if ( Roll === 5){
-//     dieString = "five";
-// }
-// else{
-//     dieString = "six";
+//     if ( Roll === 1){
+//         dieString = "one";
+//     }
+//     else if ( Roll === 2){
+//         dieString = "two";
+//     }
+//     else if ( Roll === 3){
+//         dieString = "three";
+//     }
+//     else if ( Roll === 4){
+//         dieString = "four";
+//     }
+//     else if ( Roll === 5){
+//         dieString = "five";
+//     }
+//     else{
+//         dieString = "six";
+//     }
+
+//     return dieString;
+
 // }
 
-// return dieString;
-
-// }
-
+// console.log(dieString)
 // const intToString = () => {}
